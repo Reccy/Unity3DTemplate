38c5c0956f809701a2f4ccd370dad1a8f24e00e7
Aaron Meaney <aaronmeaney@hotmail.com>
2021-09-30 16:39:56 +0100

    WIP add GitHub repo creation

--- init_template.py
+++ init_template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import subprocess
+import requests
 import pathlib
 import os
 
@@ -31,6 +32,31 @@ def prompt_str(question):
     response = input("[Unity Init] " + question + "\n > ").strip()
     return response
 
+def prompt_int(question):
+    response = input("[Unity Init] " + question + "\n > ").strip()
+
+    try:
+        response = int(response)
+    except ValueError:
+        return -1
+
+    return response
+
+def prompt_list(question, options):
+    question_str = question
+
+    length = len(options)
+    for i in range(length):
+        s = options[i]
+        question_str += "\n[Unity Init] (" + str(i + 1) + ") " + s
+
+    while True:
+        choice = prompt_int(question_str)
+        if choice > 0 and choice <= length:
+            return choice
+        else:
+            log("Please enter a valid option within the range")
+
 def clone_submodules():
     log("Initializing submodules")
     subprocess.run("git submodule init")
@@ -54,6 +80,14 @@ def set_repo_url(newUrl):
 def clear_repo_url():
     log("Clearing Repository Remote")
     subprocess.run("git remote remove origin")
+
+def attempt_github_login(username, password):
+    curl_str = "curl -u " + username + ":" + password + " https://api.github.com/" + username
+    log(curl_str)
+    subprocess.run(curl_str)
+
+def set_github_repo():
+    pass
 
 def update_readme(projectName):
     log("Generating README.MD")
@@ -95,20 +129,35 @@ def delete_file():
 def main():
     projectName = prompt_str("Please enter your project name")
 
-    if prompt("Do you have an existing empty repo you would like to push this project to?"):
+    ans = prompt_list("How would you like to handle your git repo?",
+    [
+        "Clear the remote URL and configure manually later",
+        "Set the URL to an existing empty repo",
+        "Create a new repo on GitHub"
+    ])
+
+    if ans == 2:
         repoName = prompt_str("Please enter your repo URL")
-    else:
-        repoName = None
+    elif ans == 3:
+        repoName = prompt_str("Please enter your new repo name")
+        githubUsername = prompt_str("Please enter your GitHub username")
+        githubPassword = prompt_str("Please enter your GitHub password")
+        attempt_github_login(githubUsername, githubPassword)
+
+    return
+
 
     log("Initializing Unity Project")
 
     clone_submodules()
     set_project_name(projectName)
 
-    if repoName:
-        set_repo_url(repoName)
-    else:
+    if ans == 1:
         clear_repo_url()
+    elif ans == 2:
+        set_repo_url(repoName)
+    elif ans == 3:
+        set_github_repo(repoName)
 
     delete_file()
     update_readme(projectName)
